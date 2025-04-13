31815d37e5e0e4138cbf449c899a149aa3b578f5
HarshKh13 <harshkhanna1309@gmail.com>
2025-04-12 21:55:54 -0400

    API to fetch user by username and password

--- server/api/user_handlers.go
+++ server/api/user_handlers.go
@@ -50,3 +50,24 @@ func CreateUser(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "User Created Successfully"})
 
 }
+
+func GetUserByUsernameAndPassword(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+
+	var err error
+	database, err := database.DatabaseConnection()
+	if err != nil {
+		log.Printf("Failed to connect to Database")
+	}
+
+	var user models.Users
+	result := database.Where("username=?, password=?", username, password).First(&user)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": result.Error.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"user": user,
+	})
+}
